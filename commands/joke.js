8cd878370c5c1da74fc7ba12c32b0edd2db6897a
Xeyaldi <167771989+Xeyaldi@users.noreply.github.com>
2026-04-13 19:09:02 +0400

    Update joke.js

--- commands/joke.js
+++ commands/joke.js
@@ -2,19 +2,18 @@ const axios = require('axios');
 const config = require('../config');
 
 const fallbackJokes = [
-  { setup: "Why do programmers prefer dark mode?", punchline: "Because light attracts bugs! 🐛" },
-  { setup: "Why did the developer go broke?", punchline: "Because he used up all his cache! 💰" },
-  { setup: "Why do Java developers wear glasses?", punchline: "Because they can't C#! 👓" },
-  { setup: "What's a programmer's favorite hangout place?", punchline: "Foo Bar! 🍺" },
-  { setup: "Why was the JavaScript developer sad?", punchline: "Because he didn't Node how to Express himself! 😢" },
-  { setup: "What do you call 8 hobbits?", punchline: "A hobbyte! 🧙" },
-  { setup: "Why did the developer quit his job?", punchline: "Because he didn't get arrays! 📊" },
-  { setup: "How many programmers does it take to change a light bulb?", punchline: "None, that's a hardware problem! 💡" },
+  { setup: "Pʀᴏǫʀᴀᴍçılᴀʀ ɴɪʏə ǫᴀʀᴀ ʀᴇᴊɪᴍɪ sᴇᴠɪʀ?", punchline: "Çüɴᴋɪ ɪşıǫ ʙöᴄəᴋʟəʀɪ (ʙᴜɢʟᴀʀı) ᴄəʟʙ ᴇᴅɪʀ! 🐛" },
+  { setup: "Pʀᴏǫʀᴀᴍçı ɴɪʏə ᴍüғʟɪs ᴏʟᴅᴜ?", punchline: "Çüɴᴋɪ ʙüᴛüɴ 'ᴄᴀᴄʜᴇ'ɪɴɪ (ᴋᴇşɪɴɪ) xəʀᴄləᴅɪ! 💰" },
+  { setup: "Jᴀᴠᴀ ʏᴀᴢᴀɴʟᴀʀ ɴɪʏə ᴇʏɴəᴋ ᴛᴀxıʀ?", punchline: "Çüɴᴋɪ C#'-ı (sɪ şᴀʀᴘı) ɢörə ʙɪʟᴍɪʀləʀ! 👓" },
+  { setup: "JᴀᴠᴀSᴄʀɪᴘᴛ ʏᴀᴢᴀɴ ɴɪʏə ǫəᴍʟɪ ɪᴅɪ?", punchline: "Çüɴᴋɪ öᴢüɴü ɴᴇᴄə Eхᴘʀᴇss ᴇᴅəᴄəʏɪɴɪ (ɪғᴀᴅə ᴇᴅəᴄəʏɪɴɪ) Nᴏᴅᴇ-ᴇᴛᴍɪʀᴅɪ! 😢" },
+  { setup: "Bɪʀ ᴘʀᴏǫʀᴀᴍçınıɴ sᴇᴠɪᴍʟɪ ᴍəᴋᴀɴı ʜᴀʀᴀᴅıʀ?", punchline: "Fᴏᴏ Bᴀʀ! 🍺" },
+  { setup: "8 ʜᴏʙʙɪᴛə nə ᴅᴇʏɪʟɪʀ?", punchline: "Bɪʀ ʜᴏʙʙʏᴛᴇ! 🧙" },
+  { setup: "Lᴀᴍᴘıçᴋᴀɴı ᴅəʏɪşᴍəᴋ üçüɴ ɴᴇçə ᴘʀᴏǫʀᴀᴍçı ʟᴀᴢıᴍᴅıʀ?", punchline: "Hᴇç ʙɪʀɪ, ᴏ ᴛᴇxɴɪᴋɪ (ʜᴀʀᴅᴡᴀʀᴇ) ᴘʀᴏʙʟᴇᴍᴅɪʀ! 💡" },
 ];
 
 module.exports = async (message) => {
   if (!config.features.jokes) {
-    return message.reply('Jokes feature is disabled.');
+    return message.reply('Zᴀʀᴀғᴀᴛ ғᴜɴᴋsɪʏᴀsı ᴅᴇᴀᴋᴛɪᴠᴅɪʀ. 🚫');
   }
   
   try {
@@ -23,14 +22,15 @@ module.exports = async (message) => {
     try {
       const response = await axios.get('https://official-joke-api.appspot.com/random_joke', { timeout: 5000 });
       const joke = response.data;
+      // API-ᴅəɴ ɢəʟəɴ ɪɴɢɪʟɪsᴄə ᴏʟsᴀ ᴅᴀ, sᴛɪʟɪ ǫᴏʀᴜᴅᴜᴍ
       await message.reply(`*${joke.setup}*\n\n${joke.punchline} 😄`);
     } catch {
       const joke = fallbackJokes[Math.floor(Math.random() * fallbackJokes.length)];
-      await message.reply(`*${joke.setup}*\n\n${joke.punchline}`);
+      await message.reply(`*${joke.setup}*\n\n${joke.punchline} 🤣`);
     }
     
   } catch (error) {
     console.error('Joke Error:', error.message);
-    await message.reply('Could not fetch joke. Try again!');
+    await message.reply('Zᴀʀᴀғᴀᴛ ᴛᴀᴘıʟᴍᴀᴅı, ʙɪʀ ᴀᴢ sᴏɴʀᴀ ʏᴏxʟᴀ! ⚠️');
   }
 };
